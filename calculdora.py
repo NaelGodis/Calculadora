067c7ade3fa5474e2218db0f67dbe31c8296568d
Adegilson <adegilsongs2013@gmail.com>
2022-08-09 19:54:09 -0500

    # 2 adicionar operadores básicos

--- calculdora.py
+++ calculdora.py
@@ -4,3 +4,15 @@ number_2 = int(input('Entre com o segundo número'))
 print ('{} + {} = ' .format(number_1,number_2))
 print(number_1 + number_2)
 
+
+#subtração
+print ('{} - {} = ' .format(number_1,number_2))
+print(number_1 - number_2)
+
+#multiplicação
+print ('{} * {} = ' .format(number_1,number_2))
+print(number_1 * number_2)
+
+#divisão
+print ('{} / {} = ' .format(number_1,number_2))
+print(number_1 / number_2)
