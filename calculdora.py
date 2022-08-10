ed640a2be00514d6ff060ffdb7e4e877288e1bc0
Adegilson <adegilsongs2013@gmail.com>
2022-08-10 16:04:28 -0500

    #3 permitir que o usuário  selecione a operação

--- calculdora.py
+++ calculdora.py
@@ -16,3 +16,33 @@ print(number_1 * number_2)
 #divisão
 print ('{} / {} = ' .format(number_1,number_2))
 print(number_1 / number_2)
+
+#permitir que o usuário  selecione a operação que deseja
+operation = input('''Please type in the mach operation you wold like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+number_1 = int(input('Enter your first number:'))
+number_2 = int(input('Enter your first number:'))
+
+if operation == '+':
+    print ('{} + {} =' .format(number_1,number_2))
+    print(number_1 + number_2)
+
+elif operation == '-':
+    print ('{} - {} =' .format(number_1,number_2))
+    print(number_1 - number_2)
+
+elif operation == '*':
+    print ('{} * {} =' .format(number_1,number_2))
+    print(number_1 * number_2)
+
+elif operation == '/':
+    print ('{} / {} =' .format(number_1,number_2))
+    print(number_1 / number_2)
+
+else:
+    print('You have not typed a valid operator, please run the programm again.')
