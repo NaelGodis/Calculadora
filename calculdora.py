c6bf53f2721b713f9ad87f2f3b8bdafcbcbb33a3
Adegilson <adegilsongs2013@gmail.com>
2022-08-09 19:21:57 -0500

    #1 implementar a adiçao de dois números

---
+++ calculdora.py
@@ -0,0 +1,6 @@
+#adiçao
+number_1 = int(input('Entre com o primeiro número'))
+number_2 = int(input('Entre com o segundo número'))
+print ('{} + {} = ' .format(number_1,number_2))
+print(number_1 + number_2)
+
